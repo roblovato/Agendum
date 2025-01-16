ebc7a9066f7e222aaa9fc8098e06458cfa179eb7
Rob Lovato <rlprodesign@me.com>
2025-01-16 10:59:23 -0700

    delete everything

--- Agendum/Controller/LogInController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LogInController.swift
-//  Agendum
-//
-//  Created by Rob on 12/22/20.
-//  Copyright © 2020 Rob Lovato. All rights reserved.
-//
-
-import Foundation
--- Pods/Target Support Files/Pods-Agendum/Pods-Agendum-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Agendum : NSObject
-@end
-@implementation PodsDummy_Pods_Agendum
-@end
